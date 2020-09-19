e6fe18560a36f4f31ac171a41d3bc9f1a8199a07
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-09-19 15:47:55 -0500

    testing toggle component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink} from 'reactstrap';
 import Menu from './components/Menu'
 import MenuFunction from './components/MenuFunction'
 
-function App() {
+function  App() {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
     <div>
       <Navbar dark color='primary'>
         <div className="container">
           <NavbarBrand href='/'>Coursera React Course</NavbarBrand>
         </div>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
       {/* <Menu/> */}
       <MenuFunction/>
