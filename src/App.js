95b67703adc39093adef3156be3261976fe91930
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-09-28 12:05:08 -0500

    transform class components in funtionals components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/Menu'
-import { DISHES } from './shared/dishes'
+import Main from './components/MainComponent'
+// import { Navbar, NavbarBrand } from 'reactstrap';
+// import { DISHES } from './shared/dishes'
 // import MenuFunction from './components/MenuFunction'
 
 function  App() {
 
-  const [dishes] = useState(DISHES)
-
   return (
-    <div>
-      <Navbar dark color='primary'>
-        <div className="container">
-          <NavbarBrand href='/'>Coursera React Course</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu
-        dishes={dishes}
-      />
+    <div className="App">
+      <Main />
     </div>
   );
 }
--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -1,70 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardTitle, CardBody } from "reactstrap";
 
-class Dishdetail extends Component {
-  renderComments(comments) {
-    if (comments != null) {
-      var months = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-      ];
-      const cmnts = comments.map((comment) => {
-        var d = new Date(comment.date);
-        var m = months[d.getMonth()];
-        var dt = d.getDate();
-        var y = d.getFullYear();
-        return (
-          <li key={comment.id}>
-            <p>{comment.comment}</p>
-            <p>
-              -- {comment.author},&nbsp; {m}&nbsp;{dt},&nbsp;{y}
-            </p>
-          </li>
-        );
-      });
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <h4>Comments</h4>
-          <ul className="list-unstyled">{cmnts}</ul>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
 
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
+function RenderDish({dish}) {
+  if (dish != null) {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  } else {
+    return <div></div>;
   }
-  render() {
-    const dish = this.props.dish;
+}
+
+function RenderComments({comments}) {
+  if (comments != null) {
+    var months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const cmnts = comments.map((comment) => {
+      var d = new Date(comment.date);
+      var m = months[d.getMonth()];
+      var dt = d.getDate();
+      var y = d.getFullYear();
+      return (
+        <li key={comment.id}>
+          <p>{comment.comment}</p>
+          <p>
+            -- {comment.author},&nbsp; {m}&nbsp;{dt},&nbsp;{y}
+          </p>
+        </li>
+      );
+    });
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        <ul className="list-unstyled">{cmnts}</ul>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+}
+
+const  DishDetail = (props) => {
+  const { dish } = props;
     if (dish != null) {
-      const dishItem = this.renderDish(dish);
-      const commentItem = this.renderComments(dish.comments);
+      const dishItem = RenderDish(props);
+      const commentItem = RenderComments(props.dish);
       return (
         <div className="row">
           {dishItem}
@@ -74,7 +75,6 @@ class Dishdetail extends Component {
     } else {
       return <div></div>;
     }
-  }
 }
 
-export default Dishdetail;
+export default DishDetail;
