be9a218cc551276b4407dd794d24a0ab33fb478d
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-09-28 12:42:53 -0500

    adding librearies awesome fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import Main from './components/MainComponent'
-// import { Navbar, NavbarBrand } from 'reactstrap';
-// import { DISHES } from './shared/dishes'
-// import MenuFunction from './components/MenuFunction'
 
 function  App() {
 
